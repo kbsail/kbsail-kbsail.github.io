3b25e0f68a00c7f87f83463d0ce749308c30fb13
kbsail <berry.kevinw@gmail.com>
2013-12-16 13:50:35 -0800

    updated javascript so that you can unselect a tab

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,14 +22,19 @@
 $(document).ready(function () {
   $('.tabs li').click(function(event){
     event.preventDefault();
-    $("li").removeClass('active');
-    $(this).addClass("active");    
-    $('.tab').hide();
-    $('.tab').each(function (index, element) {
-      console.log(index + ": " + $(element).attr('id'))
-    });
-    var tab_index = $(this).children('a').attr('data-tab')
-    $(tab_index).show()
-    
+      if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+        $('.tab').hide();
+      }
+      else {
+        $("li").removeClass('active');
+        $(this).addClass("active");    
+        $('.tab').hide();
+        $('.tab').each(function (index, element) {
+          console.log(index + ": " + $(element).attr('id'))
+        });
+        var tab_index = $(this).children('a').attr('data-tab')
+        $(tab_index).show()
+      }
   });
 });
