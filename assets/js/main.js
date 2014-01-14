8e2fe94df5d2b98d5badd2342c56a9f23cd940ca
kbsail <berry.kevinw@gmail.com>
2014-01-14 14:12:39 -0800

    cleaned up some JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,3 @@
-//   var Section = function(el){
-//     var self = this;
-//     this.$el = el;
-
-//     this.$el.on('click', function(e) {
-//       e.preventDefault();
-//       this.toggle();
-//     })
-//   }
-
-// $(function() {
-//   $('.section-row').hide();
-//   $('#lprojects').on('click', function(e){
-//     $('#projects').toggle();
-//   })
-//   $('#lexperience').on('click', function(e){
-//     $('#experience').toggle();
-//   })
-// });
-
-
 $(document).ready(function () {
   $('.tabs li').click(function(event){
     event.preventDefault();
@@ -30,9 +9,7 @@ $(document).ready(function () {
         $("li").removeClass('active');
         $(this).addClass("active");    
         $('.tab').hide();
-        $('.tab').each(function (index, element) {
-          console.log(index + ": " + $(element).attr('id'))
-        });
+        
         var tab_index = $(this).children('a').attr('data-tab')
         $(tab_index).show()
       }
