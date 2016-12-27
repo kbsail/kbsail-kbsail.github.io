ababcb99d2f903fb830d8dd10c2724c0b1daa8f2
Kevin Berry <berry.kevinw@gmail.com>
2016-12-27 12:48:12 -0200

    added new favicon and reactjs

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // webpack folder's entry js - excluded from jekll's build process.
+  entry: "./webpack/entry.js",
+  output: {
+    path: "src/assets/javascripts",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: "babel", // "babel-loader" is also a legal name to reference
+        query: {
+          presets: ["react", "es2015"]
+        }
+      }
+    ]
+  }
+};
